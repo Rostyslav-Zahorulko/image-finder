347fd963a07af90145802172d02e2f701e866aef
Rostyslav Zahorulko <rostyslav.zagorulko@gmail.com>
2021-01-11 23:34:34 +0200

    Пiдправляє сповiщення

--- src/js/notifications.js
+++ src/js/notifications.js
@@ -6,31 +6,27 @@ import { defaults } from '@pnotify/core';
 defaults.mode = 'light';
 defaults.styling = 'material';
 defaults.icons = 'material';
+defaults.delay = 2000;
+defaults.width = '350px';
 
 function showNotice() {
   notice({
     title: 'Warning!',
-    text: 'Unable to make a search query.\nPlease enter any text!',
-    width: '400px',
-    delay: 2000,
+    text: 'Unable to make a search query. Please enter any text!',
   });
 }
 
 function showInfo() {
   info({
     title: 'Sorry!',
-    text: 'Your search did not match any images.\nPlease clarify the request!',
-    width: '400px',
-    delay: 2000,
+    text: 'Your search did not match any images. Please clarify the request!',
   });
 }
 
 function showSuccessMessage() {
   success({
     title: 'Congratulations!',
-    text: 'Images were found on your request.\nEnjoy watching!',
-    width: '400px',
-    delay: 2000,
+    text: 'Images were found on your request. Enjoy watching!',
   });
 }
 
@@ -38,8 +34,6 @@ function showErrorMessage(err) {
   error({
     title: 'Error!',
     text: `${err}`,
-    width: '400px',
-    delay: 2000,
   });
 }
 
